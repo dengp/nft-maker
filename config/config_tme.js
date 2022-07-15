1eaffba480c1d678a1ce1d0a6ecd9420233eac9d
邓平 <ping.deng@tencentmusic.com>
2022-07-15 18:29:33 +0800

    feat(random): 权重及次数

--- config/config_tme.js
+++ config/config_tme.js
@@ -1,17 +1,19 @@
+const width = 2000; 
+const height = 2000;
+
+
 // 入口配置
 module.exports.inputConfig = {
-  projectName: 'yx_xm',
-  basePath: 'layers/yx_xm',
-  width: 2000,
-  height: 2000,
+  projectName: 'gogopand',
+  basePath: './layers',
+  baseDir: ['bg', 'face', 'glasses', 'main'],
+  decorationDir: 'other',
+  width,
+  height,
   part: [
     {
       path: 'other/0',
-      times: 20
-    },
-    {
-      path: 'other/1',
-      times: 20
+      times: 59
     }
   ]
 }
@@ -19,76 +21,87 @@ module.exports.inputConfig = {
 // 稀有度配置
 module.exports.weightConfig = {
   bg: [0.05, 0.05, 0.05, 0.05, 0.05, 0.5, 0.05, 0.05, 0.05, 0.05, 0.05],
-  face: [0.1, 0.5, 0.2, 0.2],
+  face: [0.7, 0.3],
   glasses: [0.2, 0.8],
   main: [1],
   other0: {
-    hat: [0.5, 0.25, 0.25],
-    underwear: [0.7, 0.3],
-    coat: [1],
-  },
-  other1: {
-    coat: [0.02, 0.03, 0.02, 0.02, 0.01, 0.03, 0.02, 0.05, 0.5, 0.3],
-    hat: [0.1, 0.1, 0.1, 0.1, 0.1, 0.5],
-    underwear: [0.1, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.5, 0.1],
+    coat: [0.3, 0.7],
+    hat: [0.4, 0.6],
+    underwear: [0.3, 0.2, 0.5],
   }
 }
+
+module.exports.xlsxConfig = {
+  desc: 'pandaman永不为奴',
+  title: ['序号', '名称', '描述', '发行量', '背景', '表情', '眼镜', '身体', '外套', '头饰', '内衫']
+}
+
+// 索引名称映射
+module.exports.indexToName = [
+  ['背景-0', '背景-1', '背景-2', '背景-3', '背景-4', '背景-5', '背景-6', '背景-7', '背景-8', '背景-9', '背景-10'],
+  ['表情-默认', '表情-调皮'],
+  ['眼镜-墨镜', '眼镜-平光镜'],
+  ['身体-0'],
+  ['外套-帽衫', '外套-西服'],
+  ['头饰-耳机', '头饰-鸭舌帽'],
+  ['内衫-领带衬衣款', '内衫-卫衣竹叶款', '内衫-字母款']
+]
 
 // 模型参数配置
 module.exports.base_yx = {
   other_hat: {
     x: 0,
     y: 0,
-    width: 2000,
-    height: 2000,
+    width,
+    height,
     route: 'path_other_hat',
     id: 0
   },
   other_coat: {
     x: 0,
     y: 0,
-    width: 2000,
-    height: 2000,
+    width,
+    height,
     route: 'path_other_coat',
     id: 1
   },
   other_underwear: {
     x: 0,
     y: 0,
-    width: 2000,
-    height: 2000,
+    width,
+    height,
     route: 'path_other_underwear',
     id: 2
   },
   glasses: {
     x: 0,
     y: 0,
-    width: 2000,
-    height: 2000,
+    width,
+    height,
     route: 'path_glasses',
     id: 3
   },
   face: {
     x: 0,
     y: 0,
-    width: 2000,
-    height: 2000,
+    width,
+    height,
     route: 'path_face',
     id: 4
   },
   main: {
     x: 0,
     y: 0,
-    width: 2000,
-    height: 2000,
+    width,
+    height,
     route: 'path_main',
     id: 5
   },
   bg: {
     x: 0,
     y: 0,
-    width: 2000,
-    height: 2000,
+    width,
+    height,
     route: 'path_bg',
     id: 6
   },
--- index_tme.js
+++ index_tme.js
@@ -5,7 +5,8 @@ import workerpool from 'workerpool';
 import { getWeightRandom, getRandom, getDirectories, mkdirsSync, deleteFolder } from './lib/tools';
 const pool = workerpool.pool(__dirname + '/worker/NFTWorker_tme.js');
 
-const { inputConfig, weightConfig } = require('./config/config_tme');
+const { inputConfig, weightConfig, indexToName, xlsxConfig } = require('./config/config_tme');
+const { projectName, basePath, baseDir, decorationDir, part } = inputConfig;
 
 const xlsx = require('node-xlsx');
 
@@ -14,33 +15,35 @@ Array.prototype.mySort = function() {
 }
 
 //脚本配置命令参数
-const workerNum = +process.argv[2] || os.cpus().length || 5; //开启多线程数量
+const hasWeight = +process.argv[2] || 0; //是否有权重
+const hasTimes = +process.argv[3] || 0; //是否有出现次数限制
+const workerNum = +process.argv[4] || os.cpus().length || 5; //开启多线程数量
 
 const outputDir = 'output';
-const targetDir = `${outputDir}/${inputConfig.projectName}`;
+const targetDir = `${outputDir}/${projectName}`;
 deleteFolder(targetDir);
 mkdirsSync(targetDir);
 ;(async () => {
-  for (let i = 0; i < inputConfig.part.length; i++) {
-    const eleObj = inputConfig.part[i];
+  for (let i = 0; i < part.length; i++) {
+    const eleObj = part[i];
     const { path, times } = eleObj;
-    await toMakeNft(join('./', inputConfig.basePath, path), times);
+    await toMakeNft(join('./', basePath, projectName), path, times);
   }
 })();
 
-async function toMakeNft(otherPath, times) {
+async function toMakeNft(path, otherPath, times) {
   return new Promise(async (resolve, reject) => {
     try {
       //互斥目录
-      const data_bg = getDirectories(`./layers/${inputConfig.projectName}/bg`, false).mySort();
-      const data_face = getDirectories(`./layers/${inputConfig.projectName}/face`, false).mySort();
-      const data_glasses = getDirectories(`./layers/${inputConfig.projectName}/glasses`, false).mySort();
-      const data_main = getDirectories(`./layers/${inputConfig.projectName}/main`, false).mySort();
-      const data_other = getDirectories(otherPath).mySort();
+      const data = {};
+      baseDir.forEach(ele => {
+        data[ele] = getDirectories(`${path}/${ele}`, false).mySort();
+      });
+      data.other = getDirectories(join(path, otherPath)).sort();
       // 配饰
       const data_other_data = {};
       const otherKeys = [];
-      data_other.forEach((item) => {
+      data.other.forEach((item) => {
         const key = basename(item);
         otherKeys.push(key)
         data_other_data[key] = getDirectories(item, false).mySort();
@@ -60,39 +63,53 @@ async function toMakeNft(otherPath, times) {
       }
       let curIndex = 0;
       const dataResIds = {};
+      const ids = [];
   
       //生成采样数据
       while(curIndex < times) {
-        let a = getWeightRandom(weightConfig.bg, times);
-        let b = getWeightRandom(weightConfig.face, times)
-        let c = getWeightRandom(weightConfig.glasses, times);
-        let d = getWeightRandom(weightConfig.main, times);
-        let id = `${a}_${b}_${c}_${d}_${otherIndex}`;
+        const randomArr = baseDir.map((ele) => hasWeight ? getWeightRandom(weightConfig[ele]) : getRandom(0, data[ele].length - 1))
+        let id = randomArr.join('_');
+        let [a, b, c, d] = randomArr;
         const otherRandomArr = [];
         for (let key in data_other_data) {
-          const random = getWeightRandom(weightConfig[`other${otherIndex}`][key], times);
+          const random = hasWeight ? getWeightRandom(weightConfig[`${decorationDir}${otherIndex}`][key]) : getRandom(0, data_other_data[key].length - 1);
           otherRandomArr.push(random);
           id += `_${random}`;
         }
         // 防止重复
         if(dataResIds[id]) continue;
         // 稀有度次数控制
-        if (XY.bg[a] >= weightConfig.bg[a] * times) continue;
-        if (XY.face[b] >= weightConfig.face[b] * times) continue;
-        if (XY.glasses[c] >= weightConfig.glasses[c] * times) continue;
-        if (XY.main[d] >= weightConfig.main[d] * times) continue;
-        if (XY['other']) {
-          let flag = false;
-          for (let i = 0; i < otherRandomArr.length; i++) {
-            const ele = otherRandomArr[i];
-            if(XY['other'][`${otherKeys[i]}${ele}`] >= weightConfig[`other${otherIndex}`][otherKeys[i]][ele] * times) {
-              flag = true;
+        if (hasTimes) {
+          // 基础部件
+          let baseFlag = false;
+          for (let i = 0; i < baseDir.length; i++) {
+            const key = baseDir[i];
+            const ele = randomArr[i];
+            const maxTimes = weightConfig[key][ele] * times;
+            if (XY[key][ele] === maxTimes) {
+              baseFlag = true;
+              weightConfig[key].splice(ele, 1, 0);
               break;
             }
           }
-          if (flag) continue;
+          if (baseFlag) continue;
+          // 配饰部件
+          if (XY[decorationDir]) {
+            let flag = false;
+            for (let i = 0; i < otherRandomArr.length; i++) {
+              const ele = otherRandomArr[i];
+              const key = otherKeys[i];
+              const maxTimes = weightConfig[`${decorationDir}${otherIndex}`][key][ele] * times;
+              if(XY[decorationDir][`${key}${ele}`] === maxTimes) {
+                flag = true;
+                weightConfig[`${decorationDir}${otherIndex}`][key].splice(ele, 1, 0);
+                break;
+              }
+            }
+            if (flag) continue;
+          }
         }
-        // 图片名
+        ids.push(id);
         dataResIds[id] = true;
         //特征数量纪录
         if(!XY.bg[a]) XY.bg[a] = 1;
@@ -103,22 +120,24 @@ async function toMakeNft(otherPath, times) {
         else XY.glasses[c] += 1;
         if(!XY.main[d]) XY.main[d] = 1;
         else XY.main[d] += 1;
-        if(!XY['other']) XY['other'] = {};
+        if(!XY[decorationDir]) XY[decorationDir] = {};
         otherRandomArr.forEach((ele, index) => {
-          if(!XY['other'][`${otherKeys[index]}${ele}`]) XY['other'][`${otherKeys[index]}${ele}`] = 1;
-          else XY['other'][`${otherKeys[index]}${ele}`] += 1;
+          if(!XY[decorationDir][`${otherKeys[index]}${ele}`]) XY[decorationDir][`${otherKeys[index]}${ele}`] = 1;
+          else XY[decorationDir][`${otherKeys[index]}${ele}`] += 1;
         })
         ++curIndex;
       }
       //生成NFT素材
       let runIndex = 0;
+      let workerIndex = 0;
       const resultArr = Object.keys(dataResIds).map(item => item.split('_'));
-      while(resultArr.length) {
-        await Promise.all(resultArr.splice(0, workerNum).map(async item => await run(item))); //多线程渲染
+      while(workerIndex < resultArr.length) {
+        await Promise.all(resultArr.splice(0, workerNum).map(async (item, index) => await run(item, index + 1 + workerIndex * workerNum))); //多线程渲染
+        workerIndex++;
       }
   
       //记录采样
-      createDoc(times, XY, dataResIds);
+      createDoc(times, XY, dataResIds, ids);
       
       // 结束线程
       pool.terminate(); 
@@ -129,21 +148,21 @@ async function toMakeNft(otherPath, times) {
       resolve();
 
       //执行入口
-      async function run(item) {
-        const [a, b, c, d, e, f, g, h] = item;
+      async function run(item, index) {
+        const [a, b, c, d, f, g, h] = item;
         const {hat, underwear, coat} = data_other_data;
         const id = item.join('_');
         console.log(`正在生成${id}.png`);
         await new Promise(resolve => {
           pool.exec('createNFT', ['base_yx', {
-            path_bg: data_bg[a],
-            path_face: data_face[b],
-            path_glasses: data_glasses[c],
-            path_main: data_main[d],
+            path_bg: data['bg'][a],
+            path_face: data['face'][b],
+            path_glasses: data['glasses'][c],
+            path_main: data['main'][d],
             path_other_hat: hat[g],
             path_other_underwear: underwear[h],
             path_other_coat: coat[f],
-          }, `./${targetDir}/other${otherIndex}/nft/${id}.png`])
+          }, `./${targetDir}/other${otherIndex}/nft/${projectName} #${index}.png`])
           .then(function (result) {
             ++runIndex;
             console.log(`渲染${id}.png完毕...`, runIndex);
@@ -157,21 +176,45 @@ async function toMakeNft(otherPath, times) {
       }
   
       //生成文档
-      function createDoc(total, XY, dataResIds) {
-        // console.log('XY', XY)
-        writeFileSync(`./${targetDir}/other${otherIndex}/times.json`, JSON.stringify(XY));
-        writeFileSync(`./${targetDir}/other${otherIndex}/names.json`, JSON.stringify(dataResIds));
+      function createDoc(total, XY, dataResIds, ids) {
+        // console.log('XY', XY) 
+        const { desc, title } = xlsxConfig;
+        const xlsxData = [
+          title
+        ];  
+        let index = 0;
+        let itemNames = [];
+        while(index < total) {
+          index++;
+          itemNames = ids[index - 1].split('_').map((el, i) => {
+            const names = indexToName[i] || [];
+            return names[el] || ''
+          });
+          xlsxData.push([`${index}`, `${projectName} #${index}`, desc, '1', ...itemNames])
+        }      
+        const buffer = xlsx.build([
+          {
+            name: `${projectName}-nftInfo`,
+            data: xlsxData
+          }
+        ]);
+        // xlsx
+        writeFileSync(`./${targetDir}/other${otherIndex}/${projectName}-nftInfo.xlsx`, buffer, {'flag':'w'});
+        // name json
+        writeFileSync(`./${targetDir}/other${otherIndex}/${projectName}-names.json`, JSON.stringify(dataResIds));
+        // 各部位出现次数json
+        writeFileSync(`./${targetDir}/other${otherIndex}/${projectName}-times.json`, JSON.stringify(XY));
         const textArr = [];
         textArr.push(
           `【素材总数】：${total}`,
           `【bg特征】：`,
-          `${data_bg.map((item, index) => `${index + 1}）${item.split('.')[0]}：${XY.bg[index]}`).join('\n')}`,
+          `${data.bg.map((item, index) => `${index + 1}）${item.split('.')[0]}：${XY.bg[index]}`).join('\n')}`,
           `【脸部特征】：`,
-          `${data_face.map((item, index) => `${index + 1}）${item.split('.')[0]}：${XY.face[index]}`).join('\n')}`,
+          `${data.face.map((item, index) => `${index + 1}）${item.split('.')[0]}：${XY.face[index]}`).join('\n')}`,
           `【眼镜特征】：`,
-          `${data_glasses.map((item, index) => `${index + 1}）${item.split('.')[0]}：${XY.glasses[index]}`).join('\n')}`,
+          `${data.glasses.map((item, index) => `${index + 1}）${item.split('.')[0]}：${XY.glasses[index]}`).join('\n')}`,
           `【身体特征】：`,
-          `${data_main.map((item, index) => `${index + 1}）${item.split('.')[0]}：${XY.main[index]}`).join('\n')}`,
+          `${data.main.map((item, index) => `${index + 1}）${item.split('.')[0]}：${XY.main[index]}`).join('\n')}`,
           `【帽子特征】：`,
           `${data_other_data.hat.map((item, index) => `${index + 1}）${item.split('.')[0]}：${XY.other[`hat${index}`]}`).join('\n')}`,
           `【衣服特征】：`,
@@ -179,7 +222,7 @@ async function toMakeNft(otherPath, times) {
           `【外套特征】：`,
           `${data_other_data.coat.map((item, index) => `${index + 1}）${item.split('.')[0]}：${XY.other[`coat${index}`]}`).join('\n')}`,
         );
-        writeFileSync(`./${targetDir}/other${otherIndex}/times.txt`, textArr.join('\n'));
+        writeFileSync(`./${targetDir}/other${otherIndex}/${projectName}-times.txt`, textArr.join('\n'));
       }
     } catch(e) {
       console.log('nft-creator error', e);
